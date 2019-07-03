2212c7727a27af226fa0ad5a60862cea0e90ec9d
denisson leal <denissonleal@gmail.com>
2019-07-03 13:25:37 -0300

    insert investments

---
+++ insert-investments.js
@@ -0,0 +1,1 @@
+db.investments.insert({ code: 'FLMA11', name: 'Unitas DTVM S.A.', type: 'V' })
--- update.py
+++ update.py
@@ -1,10 +1,31 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import pprint
+import pandas as pd
 
 cliente = MongoClient('172.18.0.1', 27017)
 
 db = cliente.db_03
 
-# pprint.pprint(db.investings.find())
-pprint.pprint(db.investings.find_one({ '_id': ObjectId('5d1cbe1d7e233bb6ef52616f') }))
+
+investments = db.investments.find()
+for investment in investments:
+	print(investment['code'])
+
+	df_data = pd.read_csv(
+		'data/' + investment['code'] + '.SA.csv',
+		sep=',',
+	)
+
+	df_data = df_data[['Close', 'Date']].rename(index=str, columns={'Close': 'value', 'Date': 'date'})
+
+	print(df_data.head())
+
+	investment['prices'] = df_data.to_dict(orient='records')
+
+	atual_price = df_data[df_data['date'] == df_data['date'].max()]
+
+	investment['value'] = atual_price['value'].max()
+
+	db.investments.update_one({ '_id': ObjectId(investment['_id']) }, {"$set": investment}, upsert=False)
+
