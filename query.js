306d15e215f9a54b6c11583e570cd3e3e600399c
mailtonfcarvalho <mfc2@cin.ufpe.br>
2019-07-04 14:53:51 -0300

    update query

--- query.js
+++ query.js
@@ -4,8 +4,9 @@ db.createCollection('applications')
 
 // -------------------------------------------------------------------------------------
 
-db.users.find().pretty()                    //Exibir os resultados de uma forma estruturada.
-db.users.find({},{nome:1,_id:0})            // Exibir todos os nomes do usuario sem o id
+db.users.find().pretty()                                //Exibir os resultados de uma forma estruturada.
+db.users.find({},{nome:1,_id:0})                        // Exibir todos os nomes do usuario sem o id.
+db.users.find({cpf: '123.123.123-12'}).pretty()         // Exibir os dados da pessoa cujo cpf é igual a '123.123.123-12'
 
 
 
