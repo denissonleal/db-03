4fe8fe99954f2aec238cc9ace994bf6152d7c798
mailtonfcarvalho <mfc2@cin.ufpe.br>
2019-07-04 22:42:24 -0300

    update users

--- insert-applications.js
+++ insert-applications.js
@@ -1,11 +1,10 @@
-let user = db.users.findOne({ email: "mfc2@cin.ufpe.br" })
-let investiment = db.investings.findOne({ "code" : "FLMA11" })
+let user = db.users.findOne({ email: 'mfc2@cin.ufpe.br'})
+let investment = db.investment.findOne({ 'code': 'FLMA11'})
 
 db.applications.insert({
 	user_id: user._id,
-	investment_id: investiment._id,
-	value: investiment.value,
+	investment_id: investment._id,
+	value: investment.value,
 	qtd: 10,
 	dividend: [],
 })
-
--- insert-users.js
+++ insert-users.js
@@ -3,11 +3,45 @@ db.users.insert({
 	email: 'mfc2@cin.ufpe.br',
 	cpf: '111.111.111-11',
 	born_date: '1983-02-24',
-	objectives: {quit
+	objectives: {
 		cache_back: 3000.00,
 		reservation: 3000.0*6,
 	},
 	notifications: [],
 })
 
-db.users.insert({nome: 'Maria Joaquina de Araujo',email: 'maria@gmail.com',cpf: '123.123.123-12'})
+db.users.insert({
+	nome: 'Maria Joaquina de Araujo',
+	email: 'maria@gmail.com',
+	cpf: '444.444.444-44',
+	born_date: '1991-09-12',
+	objectives: {
+		cache_back: 1500.00,
+		reservation: 2000.0*6,
+	},
+	notifications: [],
+})
+
+db.users.insert({
+	nome: 'Antonio Pedro Filho',
+	email: 'Antonio@hotmail.com',
+	cpf: '321.321.321-32',
+	born_date: '1995-10-22',
+	objectives: {
+		cache_back: 4200.00,
+		reservation: 3200.0*6,
+	},
+	notifications: [],
+})
+
+db.users.insert({
+	nome: 'Eike Batista Junior',
+	email: 'Eike@hotmail.com',
+	cpf: '999.999.999-99',
+	born_date: '1980-01-16',
+	objectives: {
+		cache_back: 10700.00,
+		reservation: 33200.0*6,
+	},
+	notifications: [],
+})
